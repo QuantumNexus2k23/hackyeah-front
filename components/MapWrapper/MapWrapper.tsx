7e631dc1a5bdf01f1e8ff8f3d6ba2e9bd75c101f
jkbstepien <devjakub.stepien@gmail.com>
2023-09-30 17:09:43 +0200

    components: add `Appbar` to `MapWrapper`

--- components/MapWrapper/MapWrapper.tsx
+++ components/MapWrapper/MapWrapper.tsx
@@ -1,10 +1,24 @@
 import React from "react";
 import { View } from "react-native";
+import { Appbar } from "react-native-paper";
+import { router } from "expo-router";
 
 type Props = { children: React.ReactNode };
 
 const MapWrapper = ({ children }: Props) => {
-  return <View>{children}</View>;
+    const handleGoBack = () => {
+        router.back();
+    };
+
+    return (
+        <View>
+            <Appbar.Header>
+                <Appbar.BackAction onPress={handleGoBack} />
+                <Appbar.Content title={'Trasa "Kraków Stare Miasto"'} />
+            </Appbar.Header>
+            {children}
+        </View>
+    );
 };
 
 export default MapWrapper;
