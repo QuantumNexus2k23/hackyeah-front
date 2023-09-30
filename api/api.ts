f144b14f172627c0e30f18320f00376c86b33d32
5ZYSZ3K <paw.por@wp.pl>
2023-09-30 19:14:54 +0200

    Add connection with backend

---
+++ api/api.ts
@@ -0,0 +1,66 @@
+import { AxiosHeaders, AxiosResponse } from "axios";
+import { client } from "./client";
+import {
+  CredentialsData,
+  RegisterData,
+  TokensData,
+} from "../stores/auth/types";
+import { MapRoute } from "../stores/mapData/types";
+
+class API {
+  async login(payload: CredentialsData): Promise<TokensData> {
+    const { data } = await this.request<TokensData>({
+      url: "/accounts/jwt/create/",
+      method: "POST",
+      payload,
+    });
+
+    return data;
+  }
+
+  async register(payload: CredentialsData): Promise<TokensData> {
+    await this.request({
+      url: "/accounts/users/",
+      method: "POST",
+      payload,
+    });
+
+    const { data } = await this.request<TokensData>({
+      url: "/accounts/jwt/create/",
+      method: "POST",
+      payload,
+    });
+
+    return data;
+  }
+
+  async getRoutes(): Promise<Array<MapRoute>> {
+    const { data } = await this.request<Array<MapRoute>>({
+      url: "/routes/",
+      method: "GET",
+    });
+
+    return data;
+  }
+
+  request<T>({
+    url,
+    method,
+    headers,
+    payload,
+  }: {
+    url: string;
+    method: "GET" | "POST";
+    headers?: AxiosHeaders;
+    payload?: any;
+  }): Promise<AxiosResponse<T>> {
+    return client.request({
+      url,
+      method,
+      headers,
+      data: payload,
+    });
+  }
+}
+
+export default new API();
--- stores/mapData/types.ts
+++ stores/mapData/types.ts
@@ -6,3 +6,19 @@ export type Coord = {
   latitude: number;
   longitude: number;
 };
+
+export type MapPoint = {
+  latitude: number;
+  longitude: number;
+};
+
+export type MapRoute = {
+  name: string;
+  description: string;
+  routePoints: Array<MapPoint>;
+};
+
+export type MapDataStore = {
+  routes: Array<MapRoute>;
+  fetchRoutes: () => Promise<void>;
+};
