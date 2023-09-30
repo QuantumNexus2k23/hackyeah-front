09837483f5e2e7bda19ca54dd5f590519feaf67c
zuzanna_bozek <zuzanna.bozek@deployed.pl>
2023-10-01 01:49:50 +0200

    style cities

--- app/maps/[slug].tsx
+++ app/maps/[slug].tsx
@@ -28,8 +28,6 @@ const Maps: FC = () => {
   const isVisitedOrNext = (index: number) =>
     isVisited(index) || index === nextStep;
 
-  console.log(route?.route_points[nextStep].short_description);
-
   return (
     <View style={{ flex: 1, paddingBottom: insets.bottom }}>
       <MapWrapper
