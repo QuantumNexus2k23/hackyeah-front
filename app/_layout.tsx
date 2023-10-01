e0ddeca13a4c86a6f174d451a1e0d6ede44bf9c7
5ZYSZ3K <60567639+5ZYSZ3K@users.noreply.github.com>
2023-10-01 09:30:30 +0200

    Eas fix (#41)

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,5 +1,4 @@
 import { Slot } from "expo-router";
-import React from "react";
 import { GestureHandlerRootView } from "react-native-gesture-handler";
 import { SafeAreaProvider } from "react-native-safe-area-context";
 import { BottomSheetModalProvider } from "@gorhom/bottom-sheet";
