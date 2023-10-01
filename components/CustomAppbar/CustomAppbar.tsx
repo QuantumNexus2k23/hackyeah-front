b2709240d770ca0ed4e5168da9888c50c774c02f
jkbstepien <devjakub.stepien@gmail.com>
2023-10-01 09:00:56 +0200

    add mock heroes view

--- components/CustomAppbar/CustomAppbar.tsx
+++ components/CustomAppbar/CustomAppbar.tsx
@@ -1,4 +1,5 @@
 import { hasCustomScheme } from "expo-linking";
+import { router } from "expo-router";
 import React from "react";
 import { Modal, Platform, StyleSheet, Text } from "react-native";
 import { Appbar, PaperProvider, Portal } from "react-native-paper";
@@ -11,11 +12,15 @@ type CustomAppbarProps = {
 const MORE_ICON = Platform.OS === "ios" ? "dots-horizontal" : "dots-vertical";
 
 const CustomAppbar = ({ title, onGoBack }: CustomAppbarProps) => {
+  const goToHeroesPage = () => {
+    router.push("/heroes");
+  };
+
   return (
     <Appbar.Header>
       <Appbar.BackAction onPress={onGoBack} />
       <Appbar.Content title={title} titleStyle={styles.titleStyle} />
-      <Appbar.Action icon={MORE_ICON} onPress={() => {}} />
+      <Appbar.Action icon={MORE_ICON} onPress={goToHeroesPage} />
     </Appbar.Header>
   );
 };
