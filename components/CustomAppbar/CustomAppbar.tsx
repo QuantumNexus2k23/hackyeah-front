683fcc210b303f9819d47a9e28fe84ff499d03ae
jkbstepien <devjakub.stepien@gmail.com>
2023-10-01 08:18:12 +0200

    add menu to appbar

--- components/CustomAppbar/CustomAppbar.tsx
+++ components/CustomAppbar/CustomAppbar.tsx
@@ -1,16 +1,21 @@
-import { StyleSheet } from "react-native";
-import { Appbar } from "react-native-paper";
+import { hasCustomScheme } from "expo-linking";
+import React from "react";
+import { Modal, Platform, StyleSheet, Text } from "react-native";
+import { Appbar, PaperProvider, Portal } from "react-native-paper";
 
 type CustomAppbarProps = {
   title: string;
   onGoBack: () => void;
 };
 
+const MORE_ICON = Platform.OS === "ios" ? "dots-horizontal" : "dots-vertical";
+
 const CustomAppbar = ({ title, onGoBack }: CustomAppbarProps) => {
   return (
     <Appbar.Header>
       <Appbar.BackAction onPress={onGoBack} />
       <Appbar.Content title={title} titleStyle={styles.titleStyle} />
+      <Appbar.Action icon={MORE_ICON} onPress={() => {}} />
     </Appbar.Header>
   );
 };
